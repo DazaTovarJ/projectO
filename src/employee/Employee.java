63064efad6c9fd12096494e26206e1e668e7fe2c
Jaime David Daza Tovar <jaimedaviddazat2001@gmail.com>
2019-11-07 15:22:42 -0500

    added Employee module

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -5,10 +5,99 @@
  */
 package employee;
 
+import java.util.Date;
+import person.Person;
+
 /**
  *
  * @author JDaza
  */
-public class Employee {
+public class Employee extends Person {
+    
+    private String position;
+    private Date dateOfEntry;
+    private Date dateOfDeparture;
+    private String contractType;
+    private String aRL;
+    private String ePS;
+    private String layoffs;
+    private String pension;
+
+    public Employee(String position, Date dateOfEntry, Date dateOfDeparture, String contractType, String aRL, String ePS, String layoffs, String pension, String givenName, String familyName, int idNumber, Date birthDate, String address, String eMail, String sex, int childrensNumber, String civilState, String profession, String educationLevel) {
+        super(givenName, familyName, idNumber, birthDate, address, eMail, sex, childrensNumber, civilState, profession, educationLevel);
+        this.position = position;
+        this.dateOfEntry = dateOfEntry;
+        this.dateOfDeparture = dateOfDeparture;
+        this.contractType = contractType;
+        this.aRL = aRL;
+        this.ePS = ePS;
+        this.layoffs = layoffs;
+        this.pension = pension;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Date getDateOfEntry() {
+        return dateOfEntry;
+    }
+
+    public void setDateOfEntry(Date dateOfEntry) {
+        this.dateOfEntry = dateOfEntry;
+    }
+
+    public Date getDateOfDeparture() {
+        return dateOfDeparture;
+    }
+
+    public void setDateOfDeparture(Date dateOfDeparture) {
+        this.dateOfDeparture = dateOfDeparture;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+
+    public String getaRL() {
+        return aRL;
+    }
+
+    public void setaRL(String aRL) {
+        this.aRL = aRL;
+    }
+
+    public String getePS() {
+        return ePS;
+    }
+
+    public void setePS(String ePS) {
+        this.ePS = ePS;
+    }
+
+    public String getLayoffs() {
+        return layoffs;
+    }
+
+    public void setLayoffs(String layoffs) {
+        this.layoffs = layoffs;
+    }
+
+    public String getPension() {
+        return pension;
+    }
+
+    public void setPension(String pension) {
+        this.pension = pension;
+    }
+    
     
 }
